41ba28b6bb87355edc045c187f3769cb538cc1df
David Clark <david.dave.clark@gmail.com>
2014-10-04 12:34:02 -0700

    Updated loader to remove unnecessary variable

--- tmpl/loader.js
+++ tmpl/loader.js
@@ -1,9 +1,8 @@
 (function (w, d) {
-    var pathToSvg = '{loaderPath}';
     if (!w.XMLHttpRequest) return false;
     w.addEventListener('DOMContentLoaded', function () {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', pathToSvg, true);
+        xhr.open('GET', '{loaderPath}', true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
                 var el = d.createElement('div');
